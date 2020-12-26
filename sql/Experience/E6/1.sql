13549ee28d059512d84ffb5d55da358ba4bb5d9b
Mr Chippy <1762307994@qq.com>
2020-12-26 18:21:03 +0800

    游标第一题

--- sql/Experience/E6/1.sql
+++ sql/Experience/E6/1.sql
@@ -1,7 +1,6 @@
-/*
-DROP PROCEDURE BankLon;
+
+DROP PROCEDURE  BankLon;
 GO
-*/
 
 CREATE PROCEDURE BankLon(
     @BankName  VARCHAR(20)
@@ -21,16 +20,32 @@ CREATE PROCEDURE BankLon(
     DECLARE @CLamount INT
     DECLARE @CLterm SMALLINT
     PRINT @BankName+'的贷款情况如下'
-    PRINT '法人名称\t贷款日期\t贷款金额(万元)\t贷款年限(年)'
+    PRINT '法人名称     贷款日期     贷款金额(万元)     贷款年限(年)'
     OPEN Bank_cur
     FETCH NEXT FROM Bank_cur INTO @CEname,@CLdate,@CLamount,@CLterm
     WHILE @@FETCH_STATUS=0
         BEGIN
-        PRINT @CEname+'\t'+CAST(@CLdate AS CHAR(10))+'\t'+CAST(@CLamount AS CHAR(6))+'\t'+CAST(@CLterm AS CHAR(3))
-        FETCH NEXT FROM Bank_cur INTO @CEname,@CLdate,@CLamount,@CLterm
+        PRINT @CEname+'     '+CAST(@CLdate AS CHAR(10))+'     '+CAST(@CLamount AS CHAR(6))+'     '+CAST(@CLterm AS CHAR(3))
+        FETCH NEXT FROM Bank_cur INTO @CEname,@CLdate,@CLamount,@CLterm;
+    END
+    CLOSE Bank_cur
+    DEALLOCATE Bank_cur;
 GO
 
 
-DECLARE @BankName VARCHAR(20)
+DECLARE @BN VARCHAR(20)
+DECLARE bankcur CURSOR FOR
+SELECT Bname FROM Bankt
+OPEN bankcur
+FETCH NEXT FROM bankcur INTO @BN
+WHILE @@FETCH_STATUS=0
+    BEGIN
+    EXEC BankLon @BankName=@BN
+    FETCH NEXT FROM bankcur INTO @BN
+    PRINT ' '
+    END 
+CLOSE Bankcur
+DEALLOCATE Bankcur;
+GO
 
 
