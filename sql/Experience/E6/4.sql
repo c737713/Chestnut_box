e001cf0033011bfcfc30fa18fdbee06680f2deec
Mr Chippy <1762307994@qq.com>
2020-12-26 21:29:55 +0800

    修改实验四的注释

--- sql/Experience/E6/4.sql
+++ sql/Experience/E6/4.sql
@@ -1,3 +1,4 @@
+/*after 触发器*/
 IF(OBJECT_ID('triggrade_after')IS NOT NULL )
 BEGIN
     DROP TRIGGER triggrade
@@ -17,7 +18,7 @@ IF(@tGrade>100)
     PRINT('成绩超过上限,修订为100')
     END;
 GO
-
+/*instead 触发器*/
 IF(OBJECT_ID('triggrade_instead')IS NOT NULL )
 BEGIN
     DROP TRIGGER triggrade
@@ -44,7 +45,7 @@ GO
 
 
 
-/*在SQL server下的instead触发器,在触发后不会执行触发其的语句*/
-/*在SQL server下的after触发器,在触发后会执行触发其的语句*/
-/*thus,instead不能同时对instead与update起效*/
+/*在SQL server下的instead触发器,在触发后不会执行触发其的语句
+在SQL server下的after触发器,在触发后会执行触发其的语句
+thus,instead不能同时对instead与update起效*/
 
